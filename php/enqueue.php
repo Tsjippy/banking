72aa5a27a16d2f1b55711a1e7b62fcc2cdf94854
Tsjippy <ewaldha@hotmail.com>
2024-10-11 07:01:02 +0100

    changed: enqueing of styles and scripts

--- php/enqueue.php
+++ php/enqueue.php
@@ -4,6 +4,6 @@ use SIM;
 
 //load js and css
 add_action( 'wp_enqueue_scripts', function(){
-    wp_register_style('sim_account_statements_style', plugins_url('css/banking.min.css', __DIR__), array(), MODULE_VERSION);
-	wp_register_script('sim_account_statements_script', plugins_url('js/account_statements.min.js', __DIR__), array(), MODULE_VERSION, true);
+    wp_register_style('sim_account_statements_style', SIM\pathToUrl(MODULE_PATH.'css/banking.min.css'), array(), MODULE_VERSION);
+	wp_register_script('sim_account_statements_script', SIM\pathToUrl(MODULE_PATH.'js/account_statements.min.js'), array(), MODULE_VERSION, true);
 });
