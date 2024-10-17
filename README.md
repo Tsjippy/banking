66e8b791aedb8fa4db32a4672cf77e0e8e41bc11
Tsjippy <ewaldha@hotmail.com>
2024-10-17 09:55:03 +0100

    changed: readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-A module for the sim-plugin to add signal messaging
+This module makes it possible to upload Account statements to the website.<br>
+These statements will be visible on the dashboard page of an user.<br>
+<br>
+This module adds one shortcode: <code>[account_statements]</code>
+<br>
+This module depends on the postie plugin and the user management module.<br>
--- php/__module_menu.php
+++ php/__module_menu.php
@@ -7,27 +7,6 @@ DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 DEFINE(__NAMESPACE__.'\STATEMENT_FOLDER', wp_get_upload_dir()["basedir"]."/private/account_statements/");
 
-add_filter('sim_submenu_description', function($description, $moduleSlug){
-	//module slug should be the same as the constant
-	if($moduleSlug != MODULE_SLUG)	{
-		return $description;
-	}
-
-	ob_start();
-	?>
-	<p>
-		This module makes it possible to upload Account statements to the website.<br>
-		These statements will be visible on the dashboard page of an user.<br>
-		<br>
-		This module adds one shortcode: <code>[account_statements]</code>
-		<br>
-		This module depends on the postie plugin and the user management module.<br>
-	</p>
-	<?php
-
-	return ob_get_clean();
-}, 10, 2);
-
 add_filter('sim_submenu_options', function($optionsHtml, $moduleSlug, $settings){
 	//module slug should be the same as grandparent folder name
 	if($moduleSlug != MODULE_SLUG){
